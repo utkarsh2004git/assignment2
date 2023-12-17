8820800a034a9ef55488e274b10c37c44ea55d91
ukarsh2004git <utkarshshahare2004@gmail.com>
2023-12-17 17:39:21 +0530

    Created Header and Footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,20 @@
-<footer>
-    <h1>This is footer</h1>
+<footer class="text-center bg-body-tertiary">
+  <div class="container pt-4">
+
+    <section class="mb-4">
+      <div class="logos  flex justify-center p-3 gap-8 rounded-xl">
+        <a href="https://github.com/utkarsh2004git" target="_blank"><img class="h-8  transition-all hover:scale-125 duration-500" src='images/github.svg'  alt="github" ></a>
+        <a href="https://www.instagram.com/utkarsh_001122/" target="_blank"><img class="h-8 transition-all hover:scale-125 duration-500" src='images/insta.svg'  alt="insta"></a>
+        <a href="https://www.linkedin.com/in/utkarsh-shahare-7b4109214/" target="_blank"><img class="h-8 transition-all hover:scale-125 duration-500" src='images/linkedin.svg'  alt="linkedin"></a>
+        <a href="https://codepen.io/utkarsh_001122" target="_blank"><img class="h-8 transition-all hover:scale-125 duration-500" src='images/codepen.svg'  alt="codepen"></a>
+        <a href="https://www.google.com" target="_blank"><img class="h-8 transition-all hover:scale-125 duration-500" src='images/google.svg'  alt="google"></a>
+    </div>
+    </section>
+  </div>
+
+  <div class="text-center p-3 font-bold hover:underline " style="background-color: rgba(0, 0, 0, 0.05);">
+    &copy; 2020 Copyright:<a class="text-body cursor-pointer" href="#">UtkarshShahare.com</a>
+
+  </div>
+
 </footer>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,3 +1,20 @@
 <header>
-    <h1 class="font-italic">This is header</h1>
+    <nav class="p-6 bg-gray-100 flex items-center  justify-between">
+            <div class="flex bg-orange-600 p-2 rounded-lg cursor-pointer hover:scale-95 transition-all ">
+                <p class="text-2xl text-semibold text-gray-200">Utkarsh</p>
+            </div>
+            <div class="font-bold text-2xl ">
+                <a href="{{ url('/posts') }}" class="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white px-6">Posts</a>
+                    @if (Route::has('login'))
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white px-6">Create</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white px-6">Log in</a>
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white px-6">Register</a>
+                        @endif
+                    @endauth
+                    @endif
+            </div>
+    </nav>
 </header>
